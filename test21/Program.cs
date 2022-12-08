13d8a0b6009e4e4963b1a7720d2704e223a925bc
DeripaskoKristina <kristallik21@mail.ru>
2022-12-09 03:11:26 +1000

    готово

--- test21/Program.cs
+++ test21/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи трёхзначное число: ");
+Random rnd = new Random();
+int number = rnd.Next(100,1000);
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
